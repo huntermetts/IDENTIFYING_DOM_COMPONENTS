4a02f4bc2665ad937a8e9a0f76778d4e73651b46
Hunter Metts <huntermettsdev@gmail.com>
2018-11-20 15:53:37 -0600

    1st problem complete, working on 2nd problem

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log("testing!");
+
+const headerEl = document.querySelector(".article__header").textContent = "Welcome to the Hunter Metts blog";
+
+const allHeaderEls = document.querySelectorAll(".article__header").classList = ("article__header important");
+
+for (var i = 0; i < allHeaderEls.length; i++) {
+   var item = allHeaderEls[i];
+  }
+
+
